e34fc418f45dbdd2d33f9ccbff2272072972ef89
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-09-26 07:09:39 +0330

    Part 1

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_healthy_shop_ui/screens/FirstScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen(),
+
+    );
+  }
+}
