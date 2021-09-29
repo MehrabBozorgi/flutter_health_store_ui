c07a50df1e426f371478192860730d024cef3610
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-09-29 07:09:22 +0330

    Part 2

---
+++ lib/model/product.dart
@@ -0,0 +1,19 @@
+class Product {
+  final int id;
+  final String title;
+  final String image;
+  final String cat;
+  final double price;
+  final String description;
+  final String point;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.cat,
+    required this.price,
+    required this.description,
+    required this.point,
+  });
+}
